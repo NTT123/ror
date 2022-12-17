637025a796a8f18f01ffbd5dbeebe765a8932126
ntt123 <ntt12@home>
2022-12-17 17:38:10 +0800

    create ORT session

--- ror-sys/build.rs
+++ ror-sys/build.rs
@@ -1,2 +1,31 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
+    let t = std::env::var("ONNX_DIR").unwrap_or(String::from(
+        "/opt/onnxruntime-linux-x64-1.13.1",
+    ));
+    let onnx_dir = PathBuf::from(t);
+    println!("cargo:rustc-link-search={}", onnx_dir.join("lib").display());
+    println!("cargo:rustc-link-lib=onnxruntime");
+    let bindings = bindgen::Builder::default()
+        .header(
+            onnx_dir
+                .join("include/onnxruntime_c_api.h")
+                .to_str()
+                .unwrap(),
+        )
+        .allowlist_type("OrtApiBase")
+        .allowlist_var("ORT_API_VERSION")
+        .allowlist_var("OrtLoggingLevel_ORT_LOGGING_LEVEL_*")
+        .allowlist_function("OrtGetApiBase")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,74 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+extern crate ror_sys;
+use ror_sys::ffi::OrtApi;
+use ror_sys::ffi::OrtEnv;
+use ror_sys::ffi::OrtLoggingLevel_ORT_LOGGING_LEVEL_VERBOSE;
+use ror_sys::ffi::OrtSession;
+use ror_sys::ffi::OrtSessionOptions;
+use ror_sys::ffi::ORT_API_VERSION;
+use std::ffi::CString;
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct ROR {
+    api: *const OrtApi,
+    env: *const OrtEnv,
+    session: *const OrtSession,
+}
+
+impl ROR {
+    pub fn new(model_path: &str) -> Self {
+        let api = unsafe {
+            let ort_api_base = *ror_sys::ffi::OrtGetApiBase();
+            let get_api_fn = ort_api_base.GetApi.expect("GetApi err");
+            let api = get_api_fn(ORT_API_VERSION);
+            api
+        };
+        if api.is_null() {
+            panic!("Cannot create ONNX runtime API");
+        };
+
+        let mut env: *mut OrtEnv = std::ptr::null_mut();
+        let env = unsafe {
+            let name = CString::new("rust-onnx-runtime").unwrap();
+            (*api).CreateEnv.expect("CreateEnv err")(
+                OrtLoggingLevel_ORT_LOGGING_LEVEL_VERBOSE,
+                name.as_ptr(),
+                &mut env,
+            );
+            env
+        };
+        if env.is_null() {
+            panic!("Cannot create ONNX env");
+        };
+
+        let mut session_opts: *mut OrtSessionOptions = std::ptr::null_mut();
+        let session_opts = unsafe {
+            (*api)
+                .CreateSessionOptions
+                .expect("CreateSessionOptions err")(&mut session_opts);
+            session_opts
+        };
+        if session_opts.is_null() {
+            panic!("Cannot create ONNX session options");
+        };
+
+        let mut session: *mut OrtSession = std::ptr::null_mut();
+        let model_path = CString::new(model_path).unwrap();
+        let session = unsafe {
+            let _ = (*api).CreateSession.expect("CreateSession err")(
+                env,
+                model_path.as_ptr(),
+                session_opts,
+                &mut session,
+            );
+            session
+        };
+        if session.is_null() {
+            panic!("Cannot create ONNX session");
+        };
+
+        ROR { api, env, session }
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +77,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let _ror = ROR::new("candy.onnx");
     }
 }
